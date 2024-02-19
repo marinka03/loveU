29fffb1754325f77d3aa89a1d25734153d1cfb74
marinka03 <97391927+marinka03@users.noreply.github.com>
2024-02-19 03:14:30 +0000

    Deploying to gh-pages from @ marinka03/loveU@7da0fcfa6bb8df48725957152f89d9b270e8493f 🚀

---
+++ index.328a6e69.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("container"),t=document.getElementById("message");e.addEventListener("click",(function(e){return t.style.opacity?t.style.opacity="0":t.style.opacity="1"}))}();
+//# sourceMappingURL=index.328a6e69.js.map
---
+++ index.fd6ca88b.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("container"),t=document.getElementById("message");e.addEventListener("click",(function(e){return t.style.opacity?t.style.opacity="0":t.style.opacity="1"}));
+//# sourceMappingURL=index.fd6ca88b.js.map
